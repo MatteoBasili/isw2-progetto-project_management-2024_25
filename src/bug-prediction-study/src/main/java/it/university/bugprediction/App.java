bb6f86bf22594b4dc91453002d83847cbd56698a
MatteoBasili <matthew2000.mb@gmail.com>
2025-10-16 17:23:35 +0200

    Passo 2: progetto Java Maven creato, dipendenze aggiunte, setup IntelliJ completato

---
+++ src/bug-prediction-study/src/main/java/it/university/bugprediction/App.java
@@ -0,0 +1,12 @@
+package it.university.bugprediction;
+
+import weka.core.Instances;
+import org.eclipse.jgit.api.Git;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Progetto ok!");
+        Git git; // solo per verificare che JGit sia importabile
+        Instances data; // verifica Weka
+    }
+}
